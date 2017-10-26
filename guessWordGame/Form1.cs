b70ec276f512b022abc3f83a860da55cb853b22e
Vladislav Antonov <lvz3rz@gmail.com>
2017-10-26 18:18:32 +0300

    Added Task class for answers and questions

--- guessWordGame/Form1.cs
+++ guessWordGame/Form1.cs
@@ -12,9 +12,43 @@ namespace guessWordGame
 {
     public partial class Form1 : Form
     {
+        //два динамічних масиви питань та відповідей
+        public List<string> questionsArray = new List<string>();
+        public List<string> answersArray = new List<string>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        class Task
+        {
+            private string answer;
+            private string quest;
+
+            //конструктор без параметрів
+            Task ()
+            {
+                this.answer = "";
+                this.quest = "";
+
+                throw new Exception("Був викликаний пустий конструктор. Відсутні дані для відображення.");
+            }
+
+            //конструктор з параметрами
+            //викликається у випадку, якщо лінь створювати масив питань і відповідей
+            Task (string newQuestion, string questAnswer)
+            {
+                this.answer = questAnswer;
+                this.quest = newQuestion;
+            }
+
+            //конструктор з параметрами
+            //передаються два індекси - індекс запитання з масиву питань і індекс відповіді на це питання
+            Task (int questID, int answerID)
+            {
+                   
+            }
         }
     }
 }
